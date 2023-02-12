aa2311620a245d6acb4ac37566589832711e879f
alaekwebartholomew <alaekwebartholomew@gmail.com>
2023-02-12 12:25:57 -0800

    first commit

---
+++ routes/app.routes.js
@@ -0,0 +1,9 @@
+const roomTypeRouter = require("./roomType.route");
+const roomRouter = require("./room.route");
+
+const basePath = "/api/v1";
+
+module.exports = (app) => {
+  app.use(`${basePath}/room-types`, roomTypeRouter);
+  app.use(`${basePath}/rooms`, roomRouter);
+};
